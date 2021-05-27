a5eb9491a072196f29740fcd6ad22d09e265476e
Tao Lin <taoroalin@gmail.com>
2021-05-27 15:58:16 -0400

    go modules

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/jg-base
+
+go 1.16
+
+require (
+	github.com/gofiber/fiber/v2 v2.10.0
+	github.com/valyala/fasthttp v1.25.0 // indirect
+)
--- main.go
+++ main.go
@@ -2,15 +2,43 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
 
-	"github.com/valyala/fasthttp"
+	"github.com/gofiber/fiber/v2"
 )
 
-func rootHandler(ctx *fasthttp.RequestCtx) {
+var (
+	dataDir = "./data"
+)
 
+func setupFile(str string) {
+	path := dataDir + str
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		ioutil.WriteFile(path, []byte("{}"), 0644)
+	}
+}
+
+func setupFolder(str string) {
+	path := dataDir + str
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		os.Mkdir(path, 0644)
+	}
 }
 
 func main() {
+	// setup data dir
+	if _, err := os.Stat(dataDir); os.IsNotExist(err) {
+		os.Mkdir(dataDir, 0644)
+	}
+
 	fmt.Println("Starting JG Base Server")
-	fasthttp.ListenAndServe(":3000", rootHandler)
+	server := fiber.New()
+
+	server.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("hi")
+	})
+
+	log.Fatal(server.Listen(":3000"))
 }
